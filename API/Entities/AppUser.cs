bd15936ffd85c658a061056bc0c578ed3be3ebb1
Ramadan <ramadan2050@hotmail.com>
2024-07-25 10:49:12 +0300

    End of section 14

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -19,10 +19,12 @@ namespace API.Entities
         public string City { get; set; }
         public string Country { get; set; }
         public List<Photo> Photos { get; set; } = new();
+        public List<UserLike> LikedByUsers { get; set; }
+        public List<UserLike> LikedUsers { get; set; }
 
-        public int GetAge()
+        /*public int GetAge()
         {
             return DateOfBirth.CalculateAge();
-        }
+        }*/
     }
 }
--- client/src/app/members/member-card/member-card.component.ts
+++ client/src/app/members/member-card/member-card.component.ts
@@ -1,6 +1,8 @@
 import { Component, Input} from '@angular/core';
 import { Member } from '../../_models/member';
 import { ViewEncapsulation } from '@angular/core';
+import { MembersService } from '../../_services/members.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-member-card',
@@ -9,5 +11,13 @@ import { ViewEncapsulation } from '@angular/core';
 })
 export class MemberCardComponent{
   @Input() member: Member | undefined;
+
+  constructor(private memberService: MembersService, private  toastr: ToastrService){}
+
+  addLike(member: Member){
+    this.memberService.addLike(member.userName).subscribe({
+      next: () => this.toastr.success('You have liked ' + member.knownAs)
+    })
+  }
 
 }
